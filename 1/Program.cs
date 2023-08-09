53089f124761472dceba854947ffe2b0f4ea010e
kurov_alexey <kurov1986aa@mail.ru>
2023-08-09 22:02:42 +0300

    Добавили программу решения задачи 1.

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// напишите программу , которая на вход принимает два числа и выдает какое из
+// них большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше,чем {number2}");
+}
+else
+{
+     Console.WriteLine($"Число {number2} больше, чем {number1}");
+}
